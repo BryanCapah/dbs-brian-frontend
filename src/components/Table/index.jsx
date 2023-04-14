31d25a34335b8b0975998ef5092cce2cbb458cfc
brian capah <briansabbath@klique.id>
2023-04-14 14:49:54 +0700

    refactor table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,5 +1,31 @@
+import { rowComponent } from "../../helpers";
+import TBody from "./TBody";
 import TContainer from "./TContainer";
+import Td from "./Td";
+import Th from "./Th";
+import THead from "./THead";
+import Tr from "./Tr";
 
-export default function Table({ children }) {
-  return <TContainer></TContainer>;
+export default function Table({ action, header, list }) {
+  if (header && list)
+    return (
+      <TContainer>
+        <THead>
+          <Tr>
+            {Object.keys(header).map((key) => (
+              <Th>{key}</Th>
+            ))}
+          </Tr>
+        </THead>
+        <TBody>
+          {list?.map((user) => (
+            <Tr>
+              {Object.keys(user)?.map((key) => (
+                <Td>{rowComponent(user[key], action)}</Td>
+              ))}
+            </Tr>
+          ))}
+        </TBody>
+      </TContainer>
+    );
 }
--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,17 +1,14 @@
 import { Button } from "@material-tailwind/react";
 import Modal from "../../components/modal";
 import Table from "../../components/Table";
-import TBody from "../../components/Table/TBody";
-import Td from "../../components/Table/Td";
-import Th from "../../components/Table/Th";
-import THead from "../../components/Table/THead";
-import Tr from "../../components/Table/Tr";
 import Title from "../../components/Title";
 import { rowComponent } from "../../helpers";
 import { useList } from "../../hooks/list.hooks";
 export default function Form() {
   const { userList, tableHeader, nav, userListIsEmpty, modal, modalHandler } =
     useList();
+  console.log(tableHeader);
+  console.log(userListIsEmpty);
   if (!userListIsEmpty)
     return (
       <div className="w-screen p-10 animate-fade-in">
@@ -21,48 +18,15 @@ export default function Form() {
           modalHandler={modalHandler}
           modal={modal}
         >
-          <Table>
-            <THead>
-              <Tr>
-                {modal?.length > 0 &&
-                  Object.keys(modal[0]).map((key) => <Th>{key}</Th>)}
-              </Tr>
-            </THead>
-            <TBody>
-              {modal?.length > 0 &&
-                modal?.map((user) => (
-                  <Tr>
-                    {Object.keys(user)?.map((key) => (
-                      <Td>{rowComponent(user[key], modalHandler)}</Td>
-                    ))}
-                  </Tr>
-                ))}
-            </TBody>
-          </Table>
+          <Table header={modal?.length > 0 && modal[0]} list={modal} />
         </Modal>
+
         <div className="flex justify-between w-full">
           <Title text="User List" />
           <Button onClick={() => nav("/form")}>Create New User</Button>
         </div>
         <br />
-        <Table>
-          <THead>
-            <Tr>
-              {Object.keys(tableHeader).map((key) => (
-                <Th>{key}</Th>
-              ))}
-            </Tr>
-          </THead>
-          <TBody>
-            {userList?.map((user) => (
-              <Tr>
-                {Object.keys(user)?.map((key) => (
-                  <Td>{rowComponent(user[key], modalHandler)}</Td>
-                ))}
-              </Tr>
-            ))}
-          </TBody>
-        </Table>
+        <Table action={modalHandler} header={tableHeader} list={userList} />
       </div>
     );
 }
